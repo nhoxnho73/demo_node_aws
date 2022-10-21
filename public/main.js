4f2c8f7d9c183700d3df44848dda57e44ff6e38f
phong le <thanhphong7up@gmail.com>
2022-10-21 11:41:42 +0700

    update code change

--- public/main.js
+++ public/main.js
@@ -24,7 +24,7 @@ deleteButton.addEventListener('click', _ => {
     method: 'delete',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
-      name: 'lê thanh phong'
+      name: 'Phong Lê'
     })
   })
     .then(res => {
